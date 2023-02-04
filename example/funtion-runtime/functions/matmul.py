c2ff483e43b5c0f39b089a4bd26a676e0a28fac1
zhaojie <8432624+wen-zhaojie@user.noreply.gitee.com>
2023-02-04 23:10:52 +0800

    Update workload

--- example/funtion-runtime/functions/matmul.py
+++ example/funtion-runtime/functions/matmul.py
@@ -4,10 +4,10 @@ import numpy as np
 def handle(input_data):
     output_data = input_data
     # 大矩阵乘法
-    a = np.random.rand(10000,10000)
-    b = np.random.rand(10000,10000)
+    a = np.random.rand(5000,5000)
+    b = np.random.rand(5000,5000)
     start_t = time.time()
-    for i in range(10):
+    for i in range(20):
         c = np.multiply(a, b)
     compute_t = time.time() - start_t
     print(f"compute_t:{compute_t}")
