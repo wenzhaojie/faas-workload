32011e563f8685c3ae3cf2d61ec6d19357401557
zhaojie <8432624+wen-zhaojie@user.noreply.gitee.com>
2023-02-15 15:41:37 +0800

    update exp1

---
+++ example/funtion-runtime/functions/linpack.py
@@ -0,0 +1,38 @@
+from numpy import matrix, linalg, random
+from time import time
+
+
+def handle(input):
+    linpack(5000)
+    return input
+
+
+def linpack(n=5000):
+    # LINPACK benchmarks
+    ops = (2.0 * n) * n * n / 3.0 + (2.0 * n) * n
+
+    # Create AxA array of random numbers -0.5 to 0.5
+    A = random.random_sample((n, n)) - 0.5
+    B = A.sum(axis=1)
+
+    # Convert to matrices
+    A = matrix(A)
+    B = matrix(B.reshape((n, 1)))
+
+    # Ax = B
+    start = time()
+    x = linalg.solve(A, B)
+    latency = time() - start
+
+    mflops = (ops * 1e-6 / latency)
+
+    result = {
+        'mflops': mflops,
+        'latency': latency
+    }
+
+    return result
+
+
+if __name__ == '__main__':
+    print(linpack(5000))
